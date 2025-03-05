7ef16b2c3bc5c03c9a6da3dd0a951d2409851f96
Chathuranga-Senanayaka <senanayakacjs@gmail.com>
2025-03-05 23:02:16 +0530

    added handleclick fn to X

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,23 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Square from "./Square";
 
 const Board = () => {
+  const [squares, setSquares] = useState(Array(9).fill(null)); //[null, null, null, null, null, null, null, null, null]
+
+  const handleClick = (i) => {
+    //get a copy of the squares array
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
+  };
+
   return (
     <div className="flex flex-col gap-3 p-6 bg-white rounded-2xl shadow-lg border border-gray-300">
-      <div className="flex gap-3">
-        <Square />
-        <Square />
-        <Square />
+      <div className="board-row flex gap-3">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
-      <div className="flex gap-3">
-        <Square />
-        <Square />
-        <Square />
+      <div className="board-row flex gap-3">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
-      <div className="flex gap-3">
-        <Square />
-        <Square />
-        <Square />
+      <div className="board-row flex gap-3">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </div>
   );
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,16 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 
-const Square = () => {
-  const [value, setValue] = useState(null);
-  const handleClick = () => {
-    setValue("X");
-  };
-
+const Square = ({ value, onSquareClick }) => {
   return (
     <button
       className="square border-2 border-gray-500 font-bold text-4xl w-24 h-24 flex items-center justify-center 
       rounded-lg shadow-md hover:bg-gray-200 transition-all duration-300 ease-in-out"
-      onClick={handleClick}
+      onClick={onSquareClick}
     >
       {value}
     </button>
